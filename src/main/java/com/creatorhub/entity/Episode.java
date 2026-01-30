ceaa2cc6681ffeac7d612aaa6214f8661b547b62
hado0123 <hado_123@naver.com>
2026-01-30 21:26:56 +0900

    [#31] refactor: member_id, episode_id unique 제약조건 추가

--- src/main/java/com/creatorhub/entity/Episode.java
+++ src/main/java/com/creatorhub/entity/Episode.java
@@ -50,7 +50,7 @@ public class Episode extends BaseEntity {
     private boolean isPublic;
 
     @Column
-    private Integer likeCount;
+    private Integer likeCount = 0;
 
     @Column
     private Integer favoriteCount;
@@ -97,15 +97,6 @@ public class Episode extends BaseEntity {
                 .isPublic(isPublic != null && isPublic)
                 .build();
     }
-
-    public void publish() { this.isPublic = true; }
-    public void unpublish() { this.isPublic = false; }
-
-    public void enableComments() { this.isCommentEnabled = true; }
-    public void disableComments() { this.isCommentEnabled = false; }
-
-    public void changeTitle(String title) { this.title = title; }
-    public void changeCreatorNote(String note) { this.creatorNote = note; }
 
     public void addManuscriptImage(ManuscriptImage image) {
         this.manuscriptImages.add(image);
